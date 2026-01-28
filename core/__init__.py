911dd8f3e65352eb6d5f9e08426aa20028c3488c
4ismael1 <201905354@p.uapa.edu.do>
2026-01-27 22:36:31 -0400

    Add licensing system with global gate

--- core/__init__.py
+++ core/__init__.py
@@ -4,10 +4,13 @@ Core module - Funcionalidades centrales del bot
 
 from core.database import database, Database
 from core.cache import cache, RedisCache
+from core.licenses import license_manager, LicenseManager
 
 __all__ = [
     "database",
     "Database",
     "cache",
     "RedisCache",
+    "license_manager",
+    "LicenseManager",
 ]
